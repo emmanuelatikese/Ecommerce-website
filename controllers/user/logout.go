73d80a7a8c7bbb395ceed79a1e44ae9fdcfa5a92
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-10-13 10:59:07 +0100

    adding

--- controllers/user/logout.go
+++ controllers/user/logout.go
@@ -1,2 +1,29 @@
 package user_controllers
 
+import (
+	"net/http"
+	"time"
+)
+
+func Logout(w http.ResponseWriter, r *http.Request){
+	access_cookies := &http.Cookie{
+		Name: "access_token",
+		Value: "",
+		Path:     "/",
+		Expires: time.Unix(0,0),
+        HttpOnly: true,
+        Secure:   true,
+        MaxAge:   -1,
+	}
+	refresh_cookies := &http.Cookie{
+		Name: "refresh_token",
+		Value: "",
+		Path:     "/",
+		Expires: time.Unix(0,0),
+        HttpOnly: true,
+        Secure:   true,
+        MaxAge:   -1,
+	}
+	http.SetCookie(w, access_cookies)
+	http.SetCookie(w, refresh_cookies)
+}
