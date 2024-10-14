8ee732b6c9a79e35fac7b17069c5bb7bc845f61a
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-10-15 00:11:33 +0100

    adding

--- routes/product/route.go
+++ routes/product/route.go
@@ -11,7 +11,9 @@ func ProductRoute(mux *mux.Router){
 	admin_middleware, protect_middleware := middlewares.AdminProtect, middlewares.ProtectRoute
 	get_all_handler := http.HandlerFunc(product_controllers.GetAllProduct)
 	create_product_handler := http.HandlerFunc(product_controllers.CreateProduct)
+	delete_product_handler := http.HandlerFunc(product_controllers.DeleteProduct)
 	mux.Handle("/product/getAll", protect_middleware(admin_middleware(get_all_handler))).Methods("GET")
 	mux.HandleFunc("/product/getFeatured", product_controllers.GetFeaturedProduct).Methods("GET")
 	mux.Handle("/product/create", protect_middleware(admin_middleware(create_product_handler))).Methods("POST")
+	mux.Handle("/product/delete/:id", protect_middleware(admin_middleware(delete_product_handler))).Methods("DELETE")
 }
