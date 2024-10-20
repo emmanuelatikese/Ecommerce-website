2d96bc33aaf0883a6d064d3e929fd2015840d000
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-10-20 12:31:00 +0100

    adding changes

--- models/user.go
+++ models/user.go
@@ -3,15 +3,15 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	Id       primitive.ObjectID `bson:"_id,omitempty"`
+	Id       primitive.ObjectID	`bson:"_id,omitempty"`
 	Username string `json:"username"`
 	Password []byte `json:"password"`
 	Email    string `json:"email"`
 	CartItem []Cart `json:"cart_item"` // quantity- 1 as default
-	Role string `json:"role"` //enum either customer or admin but customer is default
+	Role string 	`json:"role"` //enum either customer or admin but customer is default
 }
 
 type Cart struct {
-	ProductId primitive.ObjectID `json:"product_id"`
-	Quantity int	`json:"quantity"`
+	ProductId primitive.ObjectID	`json:"product_id"`
+	Quantity int					`json:"quantity"`
 }
---
+++ routes/cart/route.go
@@ -0,0 +1,21 @@
+package carts_route
+
+import (
+	cart_controller "api/controllers/cart"
+	"api/middlewares"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func CartRoute(mux *mux.Router){
+	protect_middleware := middlewares.ProtectRoute
+	addCart_handler := http.HandlerFunc(cart_controller.AddToCart)
+	removalCart_handler := http.HandlerFunc(cart_controller.RemoveProductCart)
+	updateQty_handler := http.HandlerFunc(cart_controller.UpdateQuantity)
+	//routes
+	mux.Handle("/cart/create", protect_middleware(addCart_handler)).Methods("PATCH")
+	mux.Handle("/cart/remove/{id}", protect_middleware(removalCart_handler)).Methods("PATCH")
+	mux.Handle("/cart/updateQuality/{id}", protect_middleware(updateQty_handler)).Methods("PATCH")
+
+}
