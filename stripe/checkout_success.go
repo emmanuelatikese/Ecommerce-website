eab2b86ed3bf0467fe25f553900890ed3c291366
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-10-26 18:06:39 +0100

    adding

--- stripe/checkout_success.go
+++ stripe/checkout_success.go
@@ -1,11 +1,21 @@
 package stripe_section
 
-import "net/http"
+import (
+	response "api/utils"
+	"encoding/json"
+	"net/http"
+)
 
 func CheckoutSuccess(w http.ResponseWriter, r *http.Request){
-	sessionId := r.URL.Query().Get("session_id")
-	if sessionId == ""{
-		http.Error(w, "session_id not provided", 404)
+	type newSession struct {
+		Id string `json:"sessionId"`
+	}
+	var sessionId newSession
+	err := json.NewDecoder(r.Body).Decode(&sessionId)
+	response.ErrorHandler(err, w, http.StatusBadRequest)
+	sessId := sessionId.Id
+	if sessId == ""{
+		http.Error(w, "No session Id provided", 404)
 		return
 	}
 }
