c631442ff0ce7bb13b930e645ed2351e6a673466
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-10-29 17:44:45 +0000

    adding

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+
+      <div className='flex bg-black w-screen h-screen'>
+        <p>Hello</p>
+      </div>
+
+
+  )
+}
+
+export default App
