2f55ac031036ccdc2d2fb3d6fbe53c9cc731e747
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-11-03 18:19:06 +0000

    add

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -3,6 +3,7 @@ const home = () => {
     <div className="text-marigold-200 flex flex-col justify-center items-center ">
       <h1 className=" font-bold text-4xl pt-12">Explore Our Categories</h1>
       <p className="text-white font-bold pt-2">Discover the latest trends</p>
+      
       </div>
   )
 }
