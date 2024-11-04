1a98b0277e74e58a59a547398dd9ac4aac589cfd
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-11-04 22:49:43 +0000

    adding

--- Client/src/components/slider.jsx
+++ Client/src/components/slider.jsx
@@ -25,7 +25,7 @@ const slider = () => {
         className=' w-4/5 h-96 relative top-28'
     >
     <SwiperSlide className=' w-96 h-96 rounded-lg overflow-hidden'>
-    <motion.img whileHover={{scale:1.2, borderRadius:"8px", opacity:0.8, transition:{ ease: easeOut, duration: 0.5}}} 
+    <motion.img className='opacity-90 cursor-pointer' whileHover={{scale:1.2, borderRadius:"8px", opacity:0.8, transition:{ ease: easeOut, duration: 0.5}}} 
     exit={{duration:0.5}} src={Trouser} />
     </SwiperSlide>
    
