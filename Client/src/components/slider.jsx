7a02bd946b8e9fc1102804ce8be695cbfab7ef14
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-11-04 22:18:37 +0000

    adding

--- Client/src/components/slider.jsx
+++ Client/src/components/slider.jsx
@@ -3,6 +3,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import {Pagination} from "swiper/modules"
 import Trouser from "../assets/images/trouser.jpg"
+import {easeOut, motion} from "framer-motion"
 
 const slider = () => {
   return (
@@ -23,8 +24,8 @@ const slider = () => {
         modules={[Pagination]}
         className=' w-4/5 h-96 relative top-28'
     >
-    <SwiperSlide className='w-96 h-72 flex flex-wrap rounded-lg flex-col border-solid border-2'>
-    <img src={Trouser}/>
+    <SwiperSlide >
+    <motion.img whileHover={{scale:1.2, borderRadius:8, opacity:0.8, transition:{ ease: easeOut, duration: 0.5}}} src={Trouser} className='w-96 h-96 flex flex-wrap rounded-lg flex-col '/>
     </SwiperSlide>
    
     </Swiper>
