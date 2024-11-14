cfddf21ee56c6c689763450b02c6ccdb948f6cb1
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-11-14 20:31:23 +0000

    adding

--- Client/src/components/nav.jsx
+++ Client/src/components/nav.jsx
@@ -2,15 +2,20 @@ import { Link } from "react-router-dom";
 import Home from './../pages/home';
 import { MdHomeFilled } from "react-icons/md";
 import { PiShoppingCartSimpleFill } from "react-icons/pi";
+import { IoLogOutSharp } from "react-icons/io5";
+import { IoLogInSharp } from "react-icons/io5";
+import { useState } from "react";
+
 
 const Nav = () => {
+  const [user, setUser] = useState(false)
   return (
     <div
       className="w-full sticky bg-white h-auto rounded-md p-4 shadow-slate-400 shadow-md flex flex-row flex-wrap justify-between items-center"
     >
     <h3 className="font-sans">E-commerce</h3>
 
-    <div className="flex flex-row gap-8">
+    <div className="flex flex-row gap-8 items-center justify-center">
 
     <Link to={Home}>
     <MdHomeFilled className="w-8 h-8 bg-white text-color3 shadow-md shadow-color1 rounded"/>
@@ -18,10 +23,15 @@ const Nav = () => {
 
     <Link to={Home} className="flex flex-row flex-wrap w-14 h-8 pl-1 bg-white shadow-md shadow-color1 rounded">
     <PiShoppingCartSimpleFill className="w-8 h-8 text-color3 "/>
-    <div className="w-4 h-4 mt-3 mr-0.5 text-center text-white  text-sm  bg-color2 rounded-2xl"> 5</div>
+    <div className="w-4 h-4 mt-3 mr-0.5 text-center text-white font-bold  text-sm  bg-color2 rounded-2xl"> 5</div>
+    </Link>
+    <Link className="bg-color2  text-dash  font-bold text-center p-2 text-white rounded-md shadow-md"> Dashboard </Link>
+    
+    <Link>
+        <IoLogOutSharp className="w-8 h-8 bg-color3 text-white shadow-md shadow-color1 rounded"/>
     </Link>
 
-    <div className="bg-color2  text-dash  font-bold text-center p-2 text-white rounded-md shadow"> Dashboard </div>
+  
     </div>
     </div>
   )
