05ea50bdc696f6e9819df749555def23474750ac
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-11-22 14:11:39 +0000

    adding

--- Client/src/components/categoryCards.jsx
+++ Client/src/components/categoryCards.jsx
@@ -1,11 +1,29 @@
+import { HiMiniRectangleStack } from "react-icons/hi2"
 import Trouser from "../assets/images/trouser.jpg"
+import { PiShoppingCartSimpleFill } from "react-icons/pi"
+import { ImPriceTags } from "react-icons/im"
+
 
 
 const CategoryCards = () => {
   return (
-    <div className="bg-white flex flex-col">
-        <img  src={Trouser} className="w-60"/>
-    
+    <div className="bg-white flex flex-col p-4 rounded-md">
+        <img  src={Trouser} className="w-60 rounded-md shadow-md"/>
+        <div className=" bg-white p-5 flex flex-col gap-4">
+                  <div className="flex flex-row items-center gap-5">
+                              <HiMiniRectangleStack className="w-8 h-8 text-color3"/>
+                              <p className="font-custom ">TROUSER</p>
+                  </div>
+
+                  <div className="flex flex-row items-center gap-5">
+                              <ImPriceTags className="w-8 h-8 text-color2"/>
+                              <p className="font-custom text-color2">$400</p>
+                  </div>
+                                <div className="cursor-pointer border-color2 border-2 p-2 flex flex-row items-center text-color2 font-bold gap-8"> 
+                                <PiShoppingCartSimpleFill className="w-8 h-8 text-color2"/>
+                                <p>VIEW </p>
+                                </div>
+              </div>
     </div>
   )
 }
--- Client/src/pages/category.jsx
+++ Client/src/pages/category.jsx
@@ -2,7 +2,7 @@ import CategoryCards from "../components/categoryCards"
 
 const Category = () => {
   return (
-      <div className='flex flex-col items-center'>  
+      <div className='flex flex-col items-center gap-10'>  
         <h1 className="text-4xl font-bold font-custom pt-2">Clothes Category</h1> 
             <div className='flex flex-row items-center flex-wrap gap-4'>
             <CategoryCards/>
