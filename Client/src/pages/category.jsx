e62464641409a9a7894b6f678606ad7a26088e71
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-11-25 12:45:07 +0000

    adding

--- Client/src/pages/category.jsx
+++ Client/src/pages/category.jsx
@@ -1,11 +1,17 @@
-import ProdCards from "../components/ProdCards"
+
+import CatCards from './../components/categoryCard';
 
 const Category = () => {
   return (
       <div className='flex flex-col items-center gap-10'>  
         <h1 className="text-4xl font-bold font-custom pt-2">Clothes Category</h1> 
-            <div className='flex flex-row items-center flex-wrap gap-4'>
-            <ProdCards/>
+            <div className='flex flex-row items-center flex-wrap gap-4 justify-center'>
+            <CatCards/>
+            <CatCards/>
+            <CatCards/>
+            <CatCards/>
+            <CatCards/>
+            <CatCards/>
             </div>
       </div>
   )
