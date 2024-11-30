7578560c5cf3f83ec98bc771160f54a14b09fba8
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-11-30 18:29:48 +0000

    adding

--- Client/src/pages/cart.jsx
+++ Client/src/pages/cart.jsx
@@ -9,6 +9,7 @@ const Cart = () => {
       <h1 className="text-4xl font-bold font-custom pt-4">Cart</h1>
       {IsEmpty ? <EmptyCart/> :
 
+        <div className="flex flex-row justify-center items-center gap-4 w-11/12">
       <div className=" flex flex-col justify-center items-center  w-10/12 ">
         <CartCards/>
         <CartCards/>
@@ -18,6 +19,14 @@ const Cart = () => {
         <CartCards/>
         <CartCards/>
       </div>
+
+      <div>
+        
+      
+      
+      </div>
+        </div>
+
       }
     </div>
   )
