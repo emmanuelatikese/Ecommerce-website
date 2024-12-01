9479ab401ea6c7a27293ed33a13be97944508b61
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-12-01 21:09:29 +0000

    adding

--- Client/src/pages/cart.jsx
+++ Client/src/pages/cart.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import EmptyCart from "../components/emptyCart"
 import CartCards from './../components/cartCards';
 
@@ -9,7 +10,7 @@ const Cart = () => {
       <h1 className="text-4xl font-bold font-custom pt-4">Cart</h1>
       {IsEmpty ? <EmptyCart/> :
 
-        <div className="flex flex-row justify-center items-center gap-4 w-11/12">
+        <div className="flex flex-row  w-11/12">
       <div className=" flex flex-col justify-center items-center  w-10/12 ">
         <CartCards/>
         <CartCards/>
@@ -20,10 +21,45 @@ const Cart = () => {
         <CartCards/>
       </div>
 
-      <div>
+      <div className="fixed right-10  flex flex-col gap-4 flex-wrap">
         
+          <div className="bg-white flex flex-col w-80 p-8 gap-4 rounded-md shadow-md">
+            <h3 className="font-custom">Order Summary</h3>
+          
+            <div className="flex flex-row justify-between">
+              <p>Original price</p>
+              <p>$180</p>
+            </div>
+          <hr className="text-color4"/>
+
+          <div className="flex flex-row justify-between">
+              <h3 className="font-custom">Total price</h3>
+              <p className="font-custom text-green-600">$180</p>
+            </div>
+
+            <button className="p-2 bg-color2 text-white font-bold rounded-md shadow-sm">Proceed to Checkout</button>
+            <Link className="underline font-bold flex justify-center text-green-700">
+            <p>Continue to shopping</p>
+            
+            </Link> 
+          
+            </div>
+
+
       
-      
+<div className="bg-white flex flex-col w-80 p-8 gap-4  rounded-md shadow-md">
+            <h3 className="text-dash font-bold "> Do you have a voucher or a gift card?</h3>
+          
+            <input type="text" placeholder="Enter your coupon code here...." className="bg-color4 p-2 text-dash rounded-md shadow-md outline-none font-bold"/>
+
+            <button className="p-2 bg-color2 text-white font-bold rounded-md shadow-sm">Apply Code</button>
+            <div className="flex flex-col gap-1">
+            <p className="text-dash font-bold text-gray-600">Your available coupon:</p>
+              <p className="text-dash font-bold text-gray-600">XHGJFHE235443 - 10% off </p>
+            </div>
+              
+            </div>
+
       </div>
         </div>
 
